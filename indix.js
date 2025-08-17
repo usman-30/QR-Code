7f5fe67af5a88225869e218e926f58a46cd88380
Usman Ali <youremail@example.com>
2025-08-17 15:18:59 +0500

    Text file

---
+++ indix.js
@@ -0,0 +1,28 @@
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "URL",
+      message: "Enter the text to encode:",
+    },
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qr-img.png"));
+    // Write the URL to a text file
+    fs.writeFile("URL.txt", url, (err) => {
+      if (err) throw err;
+      console.log("URL saved to URL.txt");
+    });
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
